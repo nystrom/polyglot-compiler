5398c177ec557ecafcd8a4e067cb062d3d295518
nobody <nobody@28f1f446-40f8-11de-bba7-a9cb5bbe15e6>
2003-11-20 22:55:49 +0000

    This commit was manufactured by cvs2svn to create tag 'release-1.1.0'.

---
+++ tags/release-1.1.0/polyglot/src/polyglot/ast/Lit_c.java
@@ -0,0 +1,23 @@
+package polyglot.ext.jl.ast;
+
+import polyglot.ast.*;
+import polyglot.types.*;
+import polyglot.visit.*;
+import polyglot.util.*;
+
+/**
+ * <code>Lit</code> represents any Java literal.
+ */
+public abstract class Lit_c extends Expr_c implements Lit
+{
+    public Lit_c(Position pos) {
+	super(pos);
+    }
+
+    public abstract Object objValue();
+
+    /** Get the precedence of the expression. */
+    public Precedence precedence() {
+        return Precedence.LITERAL;
+    }
+}
