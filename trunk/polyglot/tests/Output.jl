88a3d157baec51047cfba947728f8d03a2a0eeca
schong <schong@28f1f446-40f8-11de-bba7-a9cb5bbe15e6>
2005-08-31 19:55:30 +0000

    Added a testcase Output.jl which seems to make the code writer go into an infinite loop. Not sure why yet....

---
+++ trunk/polyglot/tests/Output.jl
@@ -0,0 +1,18 @@
+public class Header  {
+
+    private void jsa$html$BlockContainer$(String s, Object n) { }
+
+    public Header jsa$html$Header: /Users/nystrom/Desktop/cvsroot/polyglot/polyglot/tests/Output.jl,v 1.1 2005-08-31 19:55:30 schong Exp $(final int level,
+                                            final Object contents) {
+        {
+	    
+	    this.jsa$html$BlockContainer$("h" +
+                                           (level <
+                                              1 ? "1" : (level >
+                                                           5 ? "5" : java.lang.Integer.toString(level))),
+                                           contents);
+
+        }
+        return this;
+    }
+}
