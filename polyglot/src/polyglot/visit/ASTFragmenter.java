54312bc76a5d9bbb2c2ca3f51e2b1946d7144e75
nystrom <nystrom@28f1f446-40f8-11de-bba7-a9cb5bbe15e6>
2007-11-20 07:39:48 +0000

    Freeze the Context when storing in fragment map.

--- polyglot/src/polyglot/visit/ASTFragmenter.java
+++ polyglot/src/polyglot/visit/ASTFragmenter.java
@@ -42,13 +42,15 @@ public class ASTFragmenter extends ContextVisitor
         if (Report.should_report(Report.visit, 2))
             Report.report(2, ">> " + this + "::leave " + n);
 
+        Context c = context().freeze();
+
         if (n instanceof ClassDecl) {
             ClassDecl cd = (ClassDecl) n;
-            fragmentMap.put(cd.classDef(), new ASTFragment(parent, cd, context()));
+            fragmentMap.put(cd.classDef(), new ASTFragment(parent, cd, c));
         }
         else if (n instanceof FieldDecl) {
             FieldDecl fd = (FieldDecl) n;
-            ASTFragment fragment = new ASTFragment(parent, fd, context());
+            ASTFragment fragment = new ASTFragment(parent, fd, c);
             fragmentMap.put(fd.fieldDef(), fragment);
             if (fd.init() != null) {
                 fragmentMap.put(fd.initializerDef(), fragment);
@@ -56,28 +58,28 @@ public class ASTFragmenter extends ContextVisitor
         }
         else if (n instanceof MethodDecl) {
             MethodDecl md = (MethodDecl) n;
-            fragmentMap.put(md.methodDef(), new ASTFragment(parent, md, context()));
+            fragmentMap.put(md.methodDef(), new ASTFragment(parent, md, c));
         }
         else if (n instanceof ConstructorDecl) {
             ConstructorDecl cd = (ConstructorDecl) n;
-            fragmentMap.put(cd.constructorDef(), new ASTFragment(parent, cd, context()));
+            fragmentMap.put(cd.constructorDef(), new ASTFragment(parent, cd, c));
         }
         else if (n instanceof LocalDecl) {
             LocalDecl ld = (LocalDecl) n;
-            fragmentMap.put(ld.localDef(), new ASTFragment(parent, ld, context()));
+            fragmentMap.put(ld.localDef(), new ASTFragment(parent, ld, c));
         }
         else if (n instanceof Formal) {
             Formal f = (Formal) n;
-            fragmentMap.put(f.localDef(), new ASTFragment(parent, f, context()));
+            fragmentMap.put(f.localDef(), new ASTFragment(parent, f, c));
         }
         else if (n instanceof Initializer) {
             Initializer i = (Initializer) n;
-            fragmentMap.put(i.initializerDef(), new ASTFragment(parent, i, context()));
+            fragmentMap.put(i.initializerDef(), new ASTFragment(parent, i, c));
         }
         else if (n instanceof New) {
             New neu = (New) n;
             if (neu.anonType() != null) {
-                fragmentMap.put(neu.anonType(), new ASTFragment(parent, neu, context()));
+                fragmentMap.put(neu.anonType(), new ASTFragment(parent, neu, c));
             }
         }
         
