3d152771f2c98b35760b27f19d79346625cf77c8
nystrom <nystrom@28f1f446-40f8-11de-bba7-a9cb5bbe15e6>
2008-03-06 20:38:39 +0000

    Fixed build problem with javac.

--- polyglot/src/polyglot/types/MemberInstance.java
+++ polyglot/src/polyglot/types/MemberInstance.java
@@ -2,7 +2,7 @@ package polyglot.types;
 
 public interface MemberInstance<T extends Def> extends Use<T> {
     Flags flags();
-    MemberInstance<T> flags(Flags flags);
+    // MemberInstance<T> flags(Flags flags);
     
     ReferenceType container();
     MemberInstance<T> container(ReferenceType container);
