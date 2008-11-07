7bfaaf7847f61decfffbb8be84836ce7cb5b4454
nystrom <nystrom@28f1f446-40f8-11de-bba7-a9cb5bbe15e6>
2008-11-07 20:06:24 +0000

    Mention where we got it.

--- polyglot/src/polyglot/lex/EscapedUnicodeReader.java
+++ polyglot/src/polyglot/lex/EscapedUnicodeReader.java
@@ -3,6 +3,8 @@
  *
  * Copyright (c) 2000-2006 Polyglot project group, Cornell University
  * 
+ * Taken from the CUP website from the handwritten Lexer by Scott Ananian 
+ * <cananian@alumni.princeton.edu>.
  */
 
 package polyglot.lex;
