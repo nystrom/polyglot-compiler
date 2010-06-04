7cba0489ac830094ef5620a27d6abeb5b0bf82d5
nate.nystrom <nate.nystrom@28f1f446-40f8-11de-bba7-a9cb5bbe15e6>
2010-06-04 00:24:20 +0000

    Bump version to 3.2.0

--- polyglot/src/polyglot/frontend/JLVersion.java
+++ polyglot/src/polyglot/frontend/JLVersion.java
@@ -12,7 +12,7 @@ package polyglot.frontend;
 public class JLVersion extends polyglot.main.Version {
     public String name() { return "jl"; }
     public int major() { return 3; }
-    public int minor() { return 1; }
-    public int patch_level() { return 3; }
-    public String toString() { return "3.1.3 (2010-02-19 22:49:32)"; }
+    public int minor() { return 2; }
+    public int patch_level() { return 0; }
+    public String toString() { return "3.2.0 (2010-06-03 19:23:58)"; }
 }
