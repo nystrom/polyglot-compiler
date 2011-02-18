272a8e7161524f541da47f184b9d0a88f774eda8
vvcave@gmail.com <vvcave@gmail.com@28f1f446-40f8-11de-bba7-a9cb5bbe15e6>
2011-02-18 23:19:16 +0000

    forgot to add JL5FunctionInstance files

---
+++ trunk/polyglot/src/generics/src/polyglot/ext/jl5/types/JL5FunctionInstance.java
@@ -0,0 +1,15 @@
+package polyglot.ext.jl5.types;
+
+import polyglot.types.FunctionInstance;
+import polyglot.types.ProcedureDef;
+import polyglot.types.ProcedureInstance;
+
+
+/**
+ * A <code>ProcedureInstance</code> contains the type information for a Java
+ * procedure (either a method or a constructor).
+ */
+public interface JL5FunctionInstance<T extends ProcedureDef> extends JL5ProcedureInstance<T>, FunctionInstance<T> {
+
+	
+}
