cc9a42cdbf0eb6342587d9a1c66332245f391abe
vvcave@gmail.com <vvcave@gmail.com@28f1f446-40f8-11de-bba7-a9cb5bbe15e6>
2011-02-21 20:31:36 +0000

    Moved isImplicitCastValid code from JL5ClassType_c to JL5TypeEnv_c

--- trunk/polyglot/src/generics/src/polyglot/ext/jl5/types/JL5ClassType_c.java
+++ trunk/polyglot/src/generics/src/polyglot/ext/jl5/types/JL5ClassType_c.java
@@ -3,15 +3,14 @@ package polyglot.ext.jl5.types;
 import java.util.Iterator;
 import java.util.List;
 
-import polyglot.ast.TypeNode;
 import polyglot.types.ClassType_c;
-import polyglot.types.Type;
+import polyglot.types.Name;
 
 public abstract class JL5ClassType_c extends ClassType_c implements JL5ClassType {
 
     public abstract List enumConstants();
     
-    public EnumInstance enumConstantNamed(String name){
+    public EnumInstance enumConstantNamed(Name name){
         for(Iterator it = enumConstants().iterator(); it.hasNext();){
             EnumInstance ei = (EnumInstance)it.next();
             if (ei.name().equals(name)){
@@ -21,33 +20,4 @@ public abstract class JL5ClassType_c extends ClassType_c implements JL5ClassType
         return null;
     }
 
-    public boolean isImplicitCastValidImpl(Type toType){
-        if (isAutoUnboxingValid(toType)) return true;
-        if (toType instanceof TypeVariable){
-            return isClassToIntersectionValid(toType);
-        }
-        //FIXME
-        return super.isImplicitCastValidImpl(toType);
-    }
-
-    //FIXME
-    private boolean isClassToIntersectionValid(Type toType){
-        TypeVariable it = (TypeVariable)toType;
-        if (it.bounds() == null || it.bounds().isEmpty()) return true;
-        return ts.isImplicitCastValid(this, ((TypeNode)it.bounds().get(0)).type());
-    }
-    
-    private boolean isAutoUnboxingValid(Type toType){
-        
-        if (!toType.isPrimitive()) return false;
-        if (toType.isInt() && this.fullName().equals("java.lang.Integer")) return true;
-        if (toType.isBoolean() && this.fullName().equals("java.lang.Boolean")) return true;
-        if (toType.isByte() && this.fullName().equals("java.lang.Byte")) return true;
-        if (toType.isShort() && this.fullName().equals("java.lang.Short")) return true;
-        if (toType.isChar() && this.fullName().equals("java.lang.Character")) return true;
-        if (toType.isLong() && this.fullName().equals("java.lang.Long")) return true;
-        if (toType.isDouble() && this.fullName().equals("java.lang.Double")) return true;
-        if (toType.isFloat() && this.fullName().equals("java.lang.Float")) return true;
-        return false;
-    }
 }
