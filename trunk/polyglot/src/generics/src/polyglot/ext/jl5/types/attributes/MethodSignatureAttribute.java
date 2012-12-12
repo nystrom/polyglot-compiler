378c1ad404a04b345a88eea4aeb46bbae6843026
vvcave@gmail.com <vvcave@gmail.com@28f1f446-40f8-11de-bba7-a9cb5bbe15e6>
2012-12-12 16:48:06 +0000

    Support to store thrown types information in attributes and perform erasure on method's thrown types

--- trunk/polyglot/src/generics/src/polyglot/ext/jl5/types/attributes/MethodSignatureAttribute.java
+++ trunk/polyglot/src/generics/src/polyglot/ext/jl5/types/attributes/MethodSignatureAttribute.java
@@ -1,5 +1,6 @@
 package polyglot.ext.jl5.types.attributes;
 
+import java.util.Collections;
 import java.util.List;
 
 import polyglot.types.DerefTransform;
@@ -11,10 +12,16 @@ import polyglot.util.TransformingList;
 public class MethodSignatureAttribute {
     private Type returnType;
     private List<Type> formalTypes;
-    
+    private List<Type> thrownTypes;
+    private List<Type> typeVariablesTypes;
+    private List<List<Type>> boundsTypes;
+
     public MethodSignatureAttribute(MethodDef def) {
         returnType = def.returnType().get();
-        formalTypes = new TransformingList<Ref<? extends Type>,Type>(def.formalTypes(), new DerefTransform<Type>());        
+        formalTypes = new TransformingList<Ref<? extends Type>,Type>(def.formalTypes(), new DerefTransform<Type>());
+        thrownTypes = new TransformingList<Ref<? extends Type>,Type>(def.throwTypes(), new DerefTransform<Type>());
+        typeVariablesTypes = Collections.EMPTY_LIST;
+        boundsTypes = Collections.EMPTY_LIST;
     }
 
     public Type getReturnType() {
@@ -24,5 +31,17 @@ public class MethodSignatureAttribute {
     public List<Type> getFormalTypes() {
         return formalTypes;
     }
-   
+
+    public List<Type> getThrownTypes() {
+        return thrownTypes;
+    }
+
+    public List<Type> getTypeVariablesTypes() {
+        return typeVariablesTypes;
+    }
+    
+    public List<List<Type>> getBoundsTypes() {
+        return boundsTypes;
+    }
+
 }
