88ff740faf5b5dc12eb7c831b1ed0a3f17536549
vvcave <vvcave@gmail.com>
2013-01-30 17:10:22 +0000

    Fix dead reference

--- polyglot/src/generics/src/polyglot/ext/jl5/types/JL5MethodInstance.java
+++ polyglot/src/generics/src/polyglot/ext/jl5/types/JL5MethodInstance.java
@@ -3,6 +3,6 @@ package polyglot.ext.jl5.types;
 import polyglot.types.MethodDef;
 import polyglot.types.MethodInstance;
 
-public interface JL5MethodInstance extends JL5FunctionInstance<MethodDef>, MethodInstance, JL5MemberInstance<MethodDef> {
+public interface JL5MethodInstance extends JL5FunctionInstance<MethodDef>, MethodInstance {
     JL5MethodInstance erasure();
 }
